f7020797b4436e8aa69adae02ad01aad05fee68a
EdwinBoersema <edwinboersema@gmail.com>
2019-11-14 15:14:34 +0100

    Add link to the competition header and styled it.

--- public/js/newComp.js
+++ public/js/newComp.js
@@ -1,9 +1,15 @@
-window.onload = () => {
-
+document.addEventListener("DOMContentLoaded", () => {
+    
     function dropdown() {
         $(".ui.dropdown").dropdown();
     };
 
     dropdown();
 
-}
+    // headers.forEach(h2 => {
+    //     h2.addEventListener('click', () => {
+    //         window.location.href = h2.dataset.href;
+    //     });
+    // });
+
+});
