8701b27283506c7a63a7ed2ff3d0cb30bd03bf6e
EdwinBoersema <edwinboersema@gmail.com>
2019-11-15 11:14:37 +0100

    Add competition Route functionality and page content.

--- controllers/competitions.controller.js
+++ controllers/competitions.controller.js
@@ -31,32 +31,33 @@ exports.create = (req, res) => {
     
 }
 
-// SHOW
+// SHOW /competitions/:id
 exports.show = (req, res) => {
-    res.render("./competitions/show");
-    // Competition.findById(req.params.id, (err, competition) => {
-    //     if (err) {
-    //         console.log(err);
-    //         res.redirect("/");
-    //     } else {
-    //         res.render("./competitions/show", {
-    //             competition: competition
-    //         });
-    //     }
-    // });
+    Competition.findById(req.params.id).populate("Climbers").exec((err, competition) => {
+        if (err) {
+            console.log(err);
+            res.redirect("/");
+        } else {
+            console.log(competition.name);
+            res.render("./competitions/show", {
+                competition: competition
+            });
+        }
+    });
 }
 
 // EDIT
 exports.edit = (req, res) => {
     res.render("./competitions/edit");
-    // Competition.findById(req.params.id, (err, competition) => {
-    //     if (err) {
-    //         console.log(err);
-    //         res.redirect("/");
-    //     } else {
-    //         res.render("./competition/edit", { competition: competition });
-    //     }
-    // });
+    Competition.findById(req.params.id, (err, competition) => {
+        if (err) {
+            req.flash("error", err);
+            console.log(err);
+            res.redirect("/");
+        } else {
+            res.render("./competition/edit", { competition: competition });
+        }
+    });
 }
 
 // update
@@ -64,9 +65,11 @@ exports.update = (req, res) => {
     Competition.findByIdAndUpdate(req.params.id, (err, competition) => {
         if (err) {
             console.log(err);
-            res.redirect("/");
+            req.flash("error", err);
+            return res.redirect("/");
         } else {
-            res.redirect("/competitions/show/" + req.params.id);
+            req.flash("succes", "Competition updated!");
+            return res.redirect("/competitions/show/" + req.params.id);
         }
     });
 }
@@ -76,10 +79,11 @@ exports.delete = (req, res) => {
     Competition.findByIdAndRemove(req.params.id, (err) => {
         if (err) {
             console.log(err);
-            res.redirect("/");
+            return res.redirect("/");
         } else {
+            req.flash("succes", "Competition removed!");
             console.log("Competition removed!");
-            res.redirect("/")
+            return res.redirect("/competitions");
         }
     });
 }
